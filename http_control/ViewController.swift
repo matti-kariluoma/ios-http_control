507ba5ce589c2c2360ac712e501fa78f91a0420f
Matti Kariluoma <matti@kariluo.ma>
2014-10-29 23:43:58 -0700

    ...huh. Can't get the service to resolve, but can get most of the way there...

--- http_control/ViewController.swift
+++ http_control/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UITableViewController,UITableViewDelegate, UITableViewDataSource
+class ViewController: UITableViewController,UITableViewDelegate, UITableViewDataSource, NSNetServiceDelegate, NSNetServiceBrowserDelegate
 {
     var httpds: [String]
-    
+    var found: [NSNetService]
+    let browser: NSNetServiceBrowser
     @IBAction
     func refresh()
     {
@@ -27,7 +29,12 @@ class ViewController: UITableViewController,UITableViewDelegate, UITableViewData
                 "http://192.168.3.100:8080/",
                 "obnoxiously long title tha twill over flow at some point hopefully righgt at the edge of the screen"
             ]
+        self.found = []
+        self.browser = NSNetServiceBrowser()
         super.init(coder: aDecoder)
+        self.browser.delegate = self
+        self.browser.searchForServicesOfType(
+            "_http._tcp.", inDomain: "local")
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
@@ -48,6 +55,59 @@ class ViewController: UITableViewController,UITableViewDelegate, UITableViewData
         if let url = NSURL(string: self.httpds[indexPath.row])
         {
             UIApplication.sharedApplication().openURL(url)
+        }
+    }
+    
+    func addService(service: NSNetService)
+    {
+        self.found.append(service)
+    }
+    
+    func resolve()
+    {
+        for service in self.found
+        {
+            service.delegate = self
+            service.resolveWithTimeout(5)
+            let name = service.name
+            self.httpds.append("search \(name)")
+        }
+        self.tableView.reloadData()
+        self.found.removeAll()
+    }
+    
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didNotSearch errorDict: [NSObject : AnyObject])
+    {
+        self.httpds.append("search failed")
+        self.tableView.reloadData()
+    }
+    
+    func netServiceDidResolveAddress(sender: NSNetService)
+    {
+        self.httpds.append("found something")
+        self.tableView.reloadData()
+        let host = sender.hostName
+        let port = sender.port
+        self.httpds.append("http://\(host):\(port)/")
+        self.tableView.reloadData()
+    }
+    
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didRemoveService aNetService: NSNetService, moreComing: Bool)
+    {
+        // TODO: stop resolving
+        // TODO: delete from list
+        if (!moreComing)
+        {
+            self.resolve()
+        }
+    }
+    
+    func netServiceBrowser(aNetServiceBrowser: NSNetServiceBrowser, didFindService aNetService: NSNetService, moreComing: Bool)
+    {
+        self.addService(aNetService)
+        if (!moreComing)
+        {
+            self.resolve()
         }
     }
     
