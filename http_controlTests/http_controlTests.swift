d721e244757f29668e2a5ba32245d2fe50504f3c
Matti Kariluoma <matti@kariluo.ma>
2014-10-29 18:23:58 -0700

    Initial Commit

---
+++ http_controlTests/http_controlTests.swift
@@ -0,0 +1,36 @@
+//
+//  http_controlTests.swift
+//  http_controlTests
+//
+//  Created by Matti Kariluoma on 10/29/14.
+//  Copyright (c) 2014 Kariluoma Industries. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class http_controlTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
