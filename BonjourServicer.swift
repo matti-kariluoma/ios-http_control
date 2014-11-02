b93c7b1dc40e41d4ef89113079b5b336ef5abc89
Matti Kariluoma <matti@kariluo.ma>
2014-11-02 12:44:07 -0800

    added ipv6, not very cleanly. not tested.

--- BonjourServicer.swift
+++ BonjourServicer.swift
@@ -29,9 +29,21 @@ class BonjourServicer: NSObject, NSNetServiceDelegate
             {
                 let ptr = UnsafePointer<sockaddr_in>(address.bytes)
                 var addr = ptr.memory.sin_addr
-                let family = ptr.memory.sin_family
                 var buf = UnsafeMutablePointer<Int8>.alloc(Int(INET6_ADDRSTRLEN))
-                let ipc = inet_ntop(Int32(family), &addr, buf, __uint32_t(INET6_ADDRSTRLEN))
+                var family = ptr.memory.sin_family
+                var ipc = UnsafePointer<Int8>()
+                if family == __uint8_t(AF_INET)
+                {
+                    ipc = inet_ntop(Int32(family), &addr, buf, __uint32_t(INET6_ADDRSTRLEN))
+                }
+                else if family == __uint8_t(AF_INET6)
+                {
+                    let ptr6 = UnsafePointer<sockaddr_in6>(address.bytes)
+                    var addr6 = ptr6.memory.sin6_addr
+                    family = ptr6.memory.sin6_family
+                    ipc = inet_ntop(Int32(family), &addr6, buf, __uint32_t(INET6_ADDRSTRLEN))
+                }
+                
                 if let ip = String.fromCString(ipc)
                 {
                     ips.append(ip)
